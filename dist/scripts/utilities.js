0fde8180ef806db575ba85304a8ae6b2cad3c908
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-03-31 16:40:02 +0530

    Add(logic/Typescript): Convert utilities.js into typescript

---
+++ dist/scripts/utilities.js
@@ -0,0 +1,28 @@
+const Utilities = {
+    // To calculate factorial
+    factorial(num) {
+        if (num === 0) {
+            return 1;
+        }
+        let ans = 1;
+        for (let i = 1; i <= num; i++) {
+            ans *= i;
+        }
+        return ans;
+    },
+    // To get square and cube root of number
+    Root(num, secondFunctionality) {
+        if (secondFunctionality) {
+            return Math.cbrt(num);
+        }
+        return Math.sqrt(num);
+    },
+    // To toggle value from degree to radian
+    convertTorad(num, degFlag) {
+        if (degFlag) {
+            return num * Math.PI / 180;
+        }
+        return num;
+    }
+};
+export default Utilities;
