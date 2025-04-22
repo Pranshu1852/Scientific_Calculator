e7392ecf22ed43f71d1a31e8d2ec664c806c1288
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-04-22 13:40:25 +0530

    Fix: keyboard handler not working

--- scripts/keyboardhandler.js
+++ scripts/keyboardhandler.js
@@ -1,14 +1,12 @@
-import inputOperation from "./inputoperation.js";
-
 // To handle keyboard input
 function handleKeyboardinput(value){
     let regex=/[0-9\|\^\(\)\.\+\-\%\!\/\*]/g;
     
     if(regex.test(value)){
-        inputOperation.addToinput.call(this,value);
+        this.inputOperation.addToinput(value);
     }
     else if(value==="Backspace"){
-        inputOperation.handleBackspace.call(this);
+        this.inputOperation.handleBackspace();
     }
     else if(value==="Enter"){
         this.calculate();
