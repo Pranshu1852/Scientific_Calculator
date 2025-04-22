0035bd89a7ac768654a79fe405cde8dc3ad06593
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-04-22 14:52:45 +0530

    Update: extract scrolling behavior into separate method for better readability

--- scripts/inputoperation.js
+++ scripts/inputoperation.js
@@ -19,10 +19,14 @@ class InputOperation {
             this.displayValue+=value;
         }
 
-        if(this.display.offsetWidth<this.display.scrollWidth){
-            this.display.scrollTo(this.display.scrollWidth,0);
-        }
+        this.scrollToEnd();
     };
+
+    scrollToEnd() {
+        if (this.display.offsetWidth < this.display.scrollWidth) {
+            this.display.scrollTo(this.display.scrollWidth, 0);
+        }
+    }
 
     // To clear all values from display
     clearDisplay(){
@@ -52,48 +56,5 @@ class InputOperation {
         this.display.value = value;
     }
 }
-    
-    
-
-// const inputOperation={
-//     // To add value/operator/ERROR in display 
-//     addToinput(value){
-//         if (typeof value !== 'string') {
-//             console.error("Invalid input: Value must be a string.");
-//             return;
-//         }
-
-//         if(this.displayValue===ERROR_MESSAGE){
-//             this.displayValue=value;
-//         }
-//         else{
-//             this.displayValue+=value;
-//         }
-
-//         if(this.display.offsetWidth<this.display.scrollWidth){
-//             this.display.scrollTo(this.display.scrollWidth,0);
-//         }
-//     },
-
-//     // To clear all values from display
-//     clearDisplay(){
-//         this.displayValue="";
-//     },
-
-//     // To show ERROR in display
-//     displayError(){
-//         this.displayValue=ERROR_MESSAGE;
-//     },
-
-//     // To remove ERROR/ Last character from display
-//     handleBackspace(){
-//         if(this.displayValue===ERROR_MESSAGE || this.displayValue === ""){
-//             this.displayValue="";
-//         }
-//         else{
-//             this.displayValue=this.displayValue.substring(0,this.displayValue.length-1)
-//         }
-//     }
-// };
 
 export default InputOperation;
